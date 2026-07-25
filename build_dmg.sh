dfa7e85e7685a72cf85c04da645c6beea583bc0c
mrfragger <mrfragger@yahoo.com>
2026-07-24 19:58:56 -0700

    build_dmg.sh

---
+++ build_dmg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+APP_NAME="deselectfolders"
+VERSION="1.0"
+DMG_NAME="${APP_NAME}-${VERSION}"
+DMG_STAGING="dmg_staging"
+
+# Make sure the app is built first
+if [ ! -d "${APP_NAME}.app" ]; then
+    echo "Error: ${APP_NAME}.app not found. Run ./build.sh first."
+    exit 1
+fi
+
+rm -rf "$DMG_STAGING" "${DMG_NAME}.dmg"
+mkdir "$DMG_STAGING"
+
+cp -R "${APP_NAME}.app" "$DMG_STAGING/"
+ln -s /Applications "$DMG_STAGING/Applications"
+
+hdiutil create -volname "$APP_NAME" \
+    -srcfolder "$DMG_STAGING" \
+    -ov -format UDZO \
+    "${DMG_NAME}.dmg"
+
+rm -rf "$DMG_STAGING"
+
+echo "Created ${DMG_NAME}.dmg"
